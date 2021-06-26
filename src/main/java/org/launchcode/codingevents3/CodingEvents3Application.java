8678561e45473113c8a28b34eb69bb97879dcb5e
Daniel C <56047938+dconford@users.noreply.github.com>
2021-06-25 20:01:09 -0500

    first commit after fixing repo

---
+++ src/main/java/org/launchcode/codingevents3/CodingEvents3Application.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEvents3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEvents3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/codingevents3/CodingEvents3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.codingevents3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEvents3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

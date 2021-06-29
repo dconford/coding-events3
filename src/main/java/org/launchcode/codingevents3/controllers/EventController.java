08dcb38896f41af16420a32575b7eb20111ead68
Daniel C <56047938+dconford@users.noreply.github.com>
2021-06-29 17:44:55 -0500

    added links to static resources for table in addition to key/value.

--- src/main/java/org/launchcode/codingevents3/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents3/controllers/EventController.java
@@ -9,17 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-  private static List<String> events = new ArrayList<String>();
+ // private static List<String> events = new ArrayList<String>();
+  private static HashMap<String,String> events = new HashMap<>();
 
   @GetMapping
     public String getAllEvents(Model model) {
       model.addAttribute("events", events);
+//      model.addAllAttributes(Map<String, String> events);
     return "events/index";
     }
 
@@ -29,8 +33,9 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-      events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDescription) {
+      events.put(eventName, eventDescription);
+     // events.add(eventDescription);
     return "redirect:";
     }
 

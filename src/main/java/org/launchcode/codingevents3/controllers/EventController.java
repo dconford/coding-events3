93326e42a11e3a48ff0535979c867310a9311404
Daniel C <56047938+dconford@users.noreply.github.com>
2021-07-02 11:58:18 -0500

    added delete events form and handlers to display form and process form.

--- src/main/java/org/launchcode/codingevents3/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents3/controllers/EventController.java
@@ -20,23 +20,43 @@ public class EventController {
  //private static List<Event> events = new ArrayList<>();
 
   @GetMapping
-    public String getAllEvents(Model model) {
-      model.addAttribute("events", EventData.getAll());
+  public String getAllEvents(Model model) {
+    model.addAttribute("events", EventData.getAll());
 //      model.addAllAttributes(Map<String, String> events);
-    return "events/index";
-    }
+  return "events/index";
+  }
 
-    @GetMapping("create")
-    public String renderCreateEventForm() {
-      return "events/create";
-    }
+  @GetMapping("create")
+  public String renderCreateEventForm() {
+    return "events/create";
+  }
 
-    @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, String eventDescription) {
-     // events.add(eventDescription);
-      EventData.add(new Event(eventName, eventDescription));
+  @PostMapping("create")
+  public String createEvent(@RequestParam String eventName, String eventDescription) {
+   // events.add(eventDescription);
+    EventData.add(new Event(eventName, eventDescription));
 
-    return "redirect:";
+  return "redirect:";
+  }
+
+  @GetMapping("delete")
+  public String renderDeleteEventForm(Model model) {
+    model.addAttribute("title", "Delete Events");
+    model.addAttribute("events", EventData.getAll());
+
+  return "events/delete";
+  }
+
+  @PostMapping("delete")
+  public String deleteEvents(@RequestParam(required = false) int[] eventIds) {
+    if (eventIds != null) {
+      for (int id : eventIds) {
+        EventData.remove(id);
+      }
     }
+  return "redirect:";
+  }
+
+
 
 }
